8c736d69ef048d20c844af84c4a88ab0ce2107c3
뚝백이 <dagraac@naver.com>
2021-06-26 06:45:54 +0900

    커밋 테스트

---
+++ DagraacSystems/Scripts/Common/Singleton.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace DagraacSystems
+{
+	/// <summary>
+	/// 생성시 쓰레드 안전한 공유 클래스.
+	/// </summary>
+	public class Singleton<T> where T : Singleton<T>, new()
+	{
+		private static readonly Lazy<T> _instance = new Lazy<T>(() => new T(), true);
+		public static T instance => _instance.Value;
+	}
+}
