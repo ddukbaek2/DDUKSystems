b80a9df2789a157ae6b36734fa514d82842e81aa
뚝백이 <dagraac@naver.com>
2021-06-26 06:49:59 +0900

    Create Manager.cs

---
+++ DagraacSystems/Scripts/Common/Manager.cs
@@ -0,0 +1,13 @@
+﻿namespace DagraacSystems
+{
+	/// <summary>
+	/// 매니저.
+	/// </summary>
+	public class Manager<T> : Singleton<T> where T : Manager<T>, new()
+	{
+		public Manager()
+		{
+
+		}
+	}
+}
