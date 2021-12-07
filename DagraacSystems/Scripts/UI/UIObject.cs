7e4535190ba114f50e6e043bed2518f150b4c21c
dagraac <dagraac@naver.com>
2021-12-07 09:11:11 +0900

    UI

---
+++ DagraacSystems/Scripts/UI/UIObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DagraacSystems.UI
+{
+	public interface IUITarget
+	{
+	}
+
+	public class UIObject
+	{
+		public IUITarget Target;
+	}
+}
---
+++ DagraacSystems/Scripts/UI/UIWindow.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DagraacSystems.Scripts.UI
+{
+	public class UIWindow
+	{
+	}
+}
